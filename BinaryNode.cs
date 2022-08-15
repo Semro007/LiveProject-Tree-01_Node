1755343efcfe3856681f5fdab4a7f336bcaf14a5
Sergio Martínez <4066099+Semro007@users.noreply.github.com>
2022-08-15 11:34:38 +0200

    Solution

---
+++ BinaryNode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class BinaryNode<T>
+{
+    public T Value { get; set; }
+    public BinaryNode<T>? LeftChild { get; set; }
+    public BinaryNode<T>? RightChild { get; set; }
+
+    // Constructor
+    public BinaryNode (T pValue) {
+        Value = pValue;
+        LeftChild = null;
+        RightChild = null;
+    }
+
+    public void AddLeft(BinaryNode<T> pNewNode)
+    {
+        LeftChild = pNewNode;
+    }
+    
+    public void AddRight(BinaryNode<T> pNewNode)
+    {
+        RightChild = pNewNode;
+    }
+
+    public override String ToString()
+    {
+        return $"{Value}: {((LeftChild is null) ? "null" : LeftChild.Value)} {((RightChild is null) ? "null" : RightChild.Value)}";
+    }
+
+}
+
+public class TestBinary
+{
+    static void Main()
+    {
+        // Test NaryNode class
+        BinaryNode<String> testTree = new BinaryNode<String>("Root");
+        BinaryNode<String> nodeA = new BinaryNode<String>("A");
+        BinaryNode<String> nodeB = new BinaryNode<String>("B");
+        BinaryNode<String> nodeC = new BinaryNode<String>("C");
+        BinaryNode<String> nodeD = new BinaryNode<String>("D");
+        BinaryNode<String> nodeE = new BinaryNode<String>("E");
+        BinaryNode<String> nodeF = new BinaryNode<String>("F");
+
+        testTree.AddLeft(nodeA);
+        testTree.AddRight(nodeB);
+        nodeA.AddLeft(nodeC);
+        nodeA.AddRight(nodeD);
+        nodeB.AddRight(nodeE);
+        nodeE.AddLeft(nodeF);
+
+        // Use Queue to print node values
+        Queue<BinaryNode<String>> prtQueue = new Queue<BinaryNode<String>>();
+        prtQueue.Enqueue(testTree);
+        while (prtQueue.Count != 0)
+        {
+            BinaryNode<String> elem = prtQueue.Dequeue();
+            Console.WriteLine(elem);
+            if (elem.LeftChild is not null)
+                prtQueue.Enqueue(elem.LeftChild);
+            if (elem.RightChild is not null)
+                prtQueue.Enqueue(elem.RightChild);
+        }
+
+    }
+
+}
+
---
+++ NaryNode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class NaryNode<T>
+{
+    public T Value { get; set; }
+    public List<NaryNode<T>>? Children { get; set; }
+
+    // Constructor
+    public NaryNode (T pValue) {
+        Value = pValue;
+        Children = new List<NaryNode<T>>();
+    }
+
+    public void AddChild(NaryNode<T> pNewNode)
+    {
+        Children.Add(pNewNode);
+    }
+    
+    public override String ToString()
+    {
+        String retValue = $"{Value}:";
+        foreach (NaryNode<T> child in Children)
+            retValue += $" {child.Value}";
+        return retValue;
+    }
+
+}
+
+public class TestNary
+{
+    static void Main()
+    {
+        // Test NaryNode class
+        NaryNode<String> testTree = new NaryNode<String>("Root");
+        NaryNode<String> nodeA = new NaryNode<String>("A");
+        NaryNode<String> nodeB = new NaryNode<String>("B");
+        NaryNode<String> nodeC = new NaryNode<String>("C");
+        NaryNode<String> nodeD = new NaryNode<String>("D");
+        NaryNode<String> nodeE = new NaryNode<String>("E");
+        NaryNode<String> nodeF = new NaryNode<String>("F");
+        NaryNode<String> nodeG = new NaryNode<String>("G");
+        NaryNode<String> nodeH = new NaryNode<String>("H");
+        NaryNode<String> nodeI = new NaryNode<String>("I");
+
+        testTree.AddChild(nodeA);
+        testTree.AddChild(nodeB);
+        testTree.AddChild(nodeC);
+        nodeA.AddChild(nodeD);
+        nodeA.AddChild(nodeE);
+        nodeC.AddChild(nodeF);
+        nodeD.AddChild(nodeG);
+        nodeF.AddChild(nodeH);
+        nodeF.AddChild(nodeI);
+
+        // Use Queue to print node values
+        Queue<NaryNode<String>> prtQueue = new Queue<NaryNode<String>>();
+        prtQueue.Enqueue(testTree);
+        while (prtQueue.Count != 0)
+        {
+            NaryNode<String> elem = prtQueue.Dequeue();
+            Console.WriteLine(elem);
+            foreach (NaryNode<String> child in elem.Children)
+                prtQueue.Enqueue(child);
+        }
+
+    }
+
+}
+
